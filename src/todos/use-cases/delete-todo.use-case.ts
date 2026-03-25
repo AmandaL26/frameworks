2d37571d75a485ca5cbc0aecee4ed04da43d49b3
AmandaL26 <amanda.lima2@estudante.ifms.edu.br>
2026-03-25 12:18:58 -0400

    aula8

--- src/todos/use-cases/delete-todo.use-case.ts
+++ src/todos/use-cases/delete-todo.use-case.ts
@@ -1,17 +1,25 @@
-import { Injectable, Logger } from "@nestjs/common";
-import {DeleteTodoRepository } from "../repository";
+import { Injectable, Logger, NotFoundException } from "@nestjs/common";
+import { DeleteTodoRepository, FindTodoByIdRepository } from "../repository";
 
 @Injectable()
 export class DeleteTodoUseCase {
     constructor(
         private readonly deleteTodoRepository: DeleteTodoRepository,
+        private readonly findTodoByIdRepository: FindTodoByIdRepository,
         private readonly logger: Logger,
-    ) {}
+    ) { }
 
-    async delete(id: string) { 
+    async delete(id: string) {
         try {
             this.logger.log("Deleting todo...");
-            const todo = await this.deleteTodoRepository.delete(id);
+
+            const todo = await this.findTodoByIdRepository.findById(id);
+
+            if (!todo) {
+                throw new NotFoundException("Todo not found");
+            }
+
+            await this.deleteTodoRepository.delete(id);
             this.logger.log("Todo deleted successfully!");
             return todo;
         } catch (error) {
--- src/todos/use-cases/update-todo.use-case.ts
+++ src/todos/use-cases/update-todo.use-case.ts
@@ -1,18 +1,26 @@
-import { Injectable, Logger } from "@nestjs/common";
-import {UpdateTodoRepository } from "../repository";
+import { Injectable, Logger, NotFoundException } from "@nestjs/common";
+import { FindTodoByIdRepository, UpdateTodoRepository } from "../repository";
 import { UpdateTodoDto } from "../dto/update-todo.dto";
 
 @Injectable()
 export class UpdateTodoUseCase {
     constructor(
         private readonly updateTodoRepository: UpdateTodoRepository,
+        private readonly findTodoByIdRepository: FindTodoByIdRepository,
         private readonly logger: Logger,
-    ) {}
+    ) { }
 
-    async update(id: string, data: UpdateTodoDto) { 
+    async update(id: string, data: UpdateTodoDto) {
         try {
             this.logger.log("Updating todo...");
-            const todo = await this.updateTodoRepository.update(data, id);
+
+            const todo = await this.findTodoByIdRepository.findById(id);
+
+            if (!todo) {
+                throw new NotFoundException("Todo not found");
+            }
+
+            await this.updateTodoRepository.update(data, id);
             this.logger.log("Todo updated successfully!");
             return todo;
         } catch (error) {
