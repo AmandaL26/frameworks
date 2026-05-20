81c563def1bf157930e68664da6524a0d53bc024
AmandaL26 <amanda.lima2@estudante.ifms.edu.br>
2026-05-20 12:11:37 -0400

    aula14

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,1 +1,21 @@
-//galaxias
+import { Injectable } from "@nestjs/common";
+import { LoginDto } from "./dto/login.dto";
+import { RegisterDto } from "./dto/register.dto";
+import { RegisterUseCase } from "./use-cases/register.use-case";
+import { LoginUseCase } from "./use-cases/login.use-case";
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly registerUseCase: RegisterUseCase,
+        private readonly loginUseCase: LoginUseCase,
+    ) {}
+
+    async register(data: RegisterDto) {
+        return await this.registerUseCase.execute(data);
+    }
+
+    async login(data: LoginDto) {
+        return await this.loginUseCase.execute(data);
+    }
+}
